a168776bc910b7081c292bc7367c3c2ef529ec50
Piotr Sadowski <piotrsadowski09@gmail.com>
2019-05-20 21:10:23 +0200

    Updates

---
+++ precache-manifest.4037084df412ee665e33c2ef72d39959.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d19fac1208326b2d65e6318dfe97c3b",
+    "url": "/WeatherApp/index.html"
+  },
+  {
+    "revision": "5e9669767eb46395a187",
+    "url": "/WeatherApp/static/css/main.60c0d17d.chunk.css"
+  },
+  {
+    "revision": "2195917da6c5aa7efd84",
+    "url": "/WeatherApp/static/js/2.34c915de.chunk.js"
+  },
+  {
+    "revision": "5e9669767eb46395a187",
+    "url": "/WeatherApp/static/js/main.f27c2fc0.chunk.js"
+  },
+  {
+    "revision": "4efbdecc212de1558e2a",
+    "url": "/WeatherApp/static/js/runtime~main.3044ef82.js"
+  },
+  {
+    "revision": "955a0a7b1a97cec6371817de72d7bd75",
+    "url": "/WeatherApp/static/media/clouds-2329680_1920.955a0a7b.jpg"
+  },
+  {
+    "revision": "5c87a411b1568c65addc48a4e70c3ecc",
+    "url": "/WeatherApp/static/media/humidity.5c87a411.svg"
+  },
+  {
+    "revision": "3077681ea260cb778fff96d874f3837e",
+    "url": "/WeatherApp/static/media/logo.3077681e.svg"
+  },
+  {
+    "revision": "6985bce978c2e2870ba1d95427c83baa",
+    "url": "/WeatherApp/static/media/pressure.6985bce9.svg"
+  },
+  {
+    "revision": "a92ffa0df9b6a89c7c6e811f2d0f4db1",
+    "url": "/WeatherApp/static/media/wind-speed.a92ffa0d.svg"
+  }
+]);
